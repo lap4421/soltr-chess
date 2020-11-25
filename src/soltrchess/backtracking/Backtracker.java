fb801bfecb20f480fa56ee77e7aeef0482648f4f
Lev Paciorkowski <lap4421@g.rit.edu>
2020-11-25 17:30:47 -0500

    sending to lizzy2

--- src/soltrchess/backtracking/Backtracker.java
+++ src/soltrchess/backtracking/Backtracker.java
@@ -16,7 +16,6 @@ import java.util.Optional;
  * 
  * @author sps (Sean Strout @ RIT CS)
  * @author jeh (James Heliotis @ RIT CS)
- *
  */
 public class Backtracker {
     
@@ -28,18 +27,24 @@ public class Backtracker {
      */
     public Optional<Configuration> solve(Configuration config) {
         if (config.isGoal()) {
+            System.out.println("Returned basic sol");
             return Optional.of(config);
         } else {
             for (Configuration child : config.getSuccessors()) {
+                System.out.println("Current config: ");
+                System.out.println(child);
                 if (child.isValid()) {
+                    System.out.println("Child is valid");
                     Optional<Configuration> sol = solve(child);
                     if (sol.isPresent()) {
+                        System.out.println("SOLUTION FOUND!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
                         return sol;
                     }
                 }
             }
             // implicit solitarechess.backtracking happens here
-        } 
+        }
+        System.out.println("Solution not found :(");
         return Optional.empty();
     }
 
